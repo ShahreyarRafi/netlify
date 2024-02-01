064ebc16a09bf750021c37614eac9f0706bd639c
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-02-01 16:33:08 +0600

    attempt to fix netlify error

--- next.config.js
+++ next.config.js
@@ -12,6 +12,25 @@ const nextConfig = {
       "img.icons8.com",
     ],
   },
+  webpack: (config, { isServer }) => {
+    // Modify the CSS loader configuration to handle CSS modules without styles object
+    config.module.rules.forEach((rule) => {
+      if (rule.oneOf) {
+        rule.oneOf.forEach((oneOfRule) => {
+          if (oneOfRule.test && oneOfRule.test.toString().includes('.module.css')) {
+            // Disable the CSS modules configuration for the standard import syntax
+            delete oneOfRule.modules;
+
+            // Add a new loader to handle the standard import syntax
+            oneOfRule.use.unshift('style-loader', 'css-loader');
+          }
+        });
+      }
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
+
